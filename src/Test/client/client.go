5d7bd75fa4b4937e7a5a0945e60b6d7f88d5dc88
seok ki <seokki@seok-2.local>
2017-02-05 07:20:20 +0900

    server connection, data send isolation

--- src/Test/client/client.go
+++ src/Test/client/client.go
@@ -8,16 +8,7 @@ import (
 	"net"
 )
 
-func sendSimulationData(airthreats []csvlib.Airthreat) {
-	conn, err := net.Dial("tcp", "127.0.0.1:8000")
-	if err != nil {
-		log.Println("서버에 연결할 수 없습니다.")
-	}
-
-	defer conn.Close()
-
-	fmt.Println("서버에 연결되었습니다.")
-
+func sendSimulationData(airthreats []csvlib.Airthreat, conn net.Conn) {
 	enc := gob.NewEncoder(conn)
 	if err := enc.Encode(airthreats); err != nil {
 		fmt.Println(err)
@@ -25,10 +16,23 @@ func sendSimulationData(airthreats []csvlib.Airthreat) {
 	}
 
 	fmt.Println("데이터 전송 완료")
+
+	defer conn.Close()
+}
+
+func connectServer() (conn net.Conn) {
+	conn, err := net.Dial("tcp", "127.0.0.1:8000")
+	if err != nil {
+	 	log.Println("서버에 연결할 수 없습니다.")
+	 }
+
+	fmt.Println("서버에 연결되었습니다.")
+
+	return conn
 }
 
 func main() {
 
 	airthreats := csvlib.ReadScenarioFile()
-	sendSimulationData(airthreats)
+	sendSimulationData(airthreats, connectServer())
 }
